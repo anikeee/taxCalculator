ecb450b3b760f94c3376f5ab8f2648c455bbf2bd
anikeee <muid.hasan@gmail.com>
2022-03-26 20:54:27 +0600

    starting new branch for Mukim

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [\App\Http\Controllers\homeController::class, 'index'])->name('dashboard');
-Route::post('/calculate-tax', [\App\Http\Controllers\homeController::class, 'calculateTax'])->name('calculate.tax');
-
